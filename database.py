5072a025f1c48e846cbb69ee098b1991a6425036
aldybw <61458257+aldybw@users.noreply.github.com>
2023-04-20 00:12:46 +0000

    dynamic changes for each job - 2

--- database.py
+++ database.py
@@ -11,9 +11,20 @@ engine = create_engine(db_connection_string,
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    for row in result:
-      print("jobs:", row)
+    result = conn.execute(text("SELECT * FROM jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(row._asdict())
+    return jobs
 
 
-load_jobs_from_db()
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    stmt = text("SELECT * FROM jobs WHERE id=:val")
+    stmt = stmt.bindparams(val=id)
+    result = conn.execute(stmt)
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
